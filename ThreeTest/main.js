50872efb559b75744fdf8a0e919bcbc012a2caa6
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2024-04-08 13:56:35 +0630

    added

--- ThreeTest/main.js
+++ ThreeTest/main.js
@@ -42,16 +42,40 @@ scene.add(plane)
 plane.rotation.x = -0.5 * Math.PI
 
 // Sphere
-const sphereGeometry = new THREE.SphereGeometry(4,50,50)
+const sphereGeometry = new THREE.SphereGeometry(4,30,30)
 const sphereMaterial = new THREE.MeshBasicMaterial({
   color:0xFFC158,
   wireframe:false
 })
 const sphere = new THREE.Mesh(sphereGeometry,sphereMaterial)
 scene.add(sphere)
-sphere.position.set(-5,5,0)
+sphere.position.set(-5,-5,0)
 
+// GUI
 const gui = new dat.GUI()
+const options = {
+  sphereColor: '#FFC158',
+  wireframe:false,
+  speed:0.01,
+  widthSegments: 30
+}
+
+gui.addColor(options,'sphereColor').onChange((e)=>{
+  sphere.material.color.set(e)
+})
+
+gui.add(options,'wireframe').onChange((e)=>{
+  sphere.material.wireframe = e
+})
+
+gui.add(options,'speed',0,0.1)
+gui.add(options,'widthSegments',1,30).onChange((e)=>{
+  sphere.geometry.parameters.widthSegments = e
+  sphere.geometry.parameters.heightSegments = e
+  sphere.geometry.dispose(); // Dispose old geometry to avoid memory leaks
+  const newGeometry = new THREE.SphereGeometry(4,e,e)
+  sphere.geometry = newGeometry;
+})
 
 const gridHelper = new THREE.GridHelper(30,10)
 scene.add(gridHelper)
@@ -59,9 +83,19 @@ scene.add(gridHelper)
 box.rotation.x = 5
 box.rotation.y = 5
 
+// Animation
+let stop = 0
+
 var animate = (time)=>{
   box.rotation.x = time/1000
   box.rotation.y = time/1000
+
+  sphere.rotation.x = time/1000
+  sphere.rotation.y = time/1000
+
+  stop += options.speed
+  sphere.position.y = 10* Math.abs(Math.sin(stop))
+
   renderer.render(scene,camera)
 }
 renderer.setAnimationLoop(animate)
